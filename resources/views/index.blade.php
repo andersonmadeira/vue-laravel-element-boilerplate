ec45f3b61ee12d63ca54bdaaaf0d023cc0f57039
Njogu Amos <njoguamos@gmail.com>
2018-09-09 02:04:23 +0300

    Updated base css and js
    * Removed bootstrap glyphicons
    * Removed bootstrap and jQuery js requirements
    * Removed bootstrap sass requirements

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
-  <title>Element App</title>
+  <title>Element UI Laraval App Boilerplate</title>
 </head>
 <body>
 <div id="mute"></div>
